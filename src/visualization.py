f63f641d0c466b8b85c9e957b59ef2dac80e6007
lucjonker <lucjonkerwork@gmail.com>
2026-05-01 11:57:27 +0200

    Update evaluator

--- src/visualization.py
+++ src/visualization.py
@@ -1,3 +1,5 @@
+import numpy as np
+import rasterio
 from matplotlib import pyplot as plt
 
 
@@ -14,30 +16,34 @@ def plot_losses(g_losses, d_losses, title="Loss Analysis:"):
     plt.show()
 
 
-# Plots result and target images assuming they come from the gpu
-def display_res(source, result, target):
-    source_cpu = source.cpu()
-    result_cpu = result.cpu().detach()
-    target_cpu = target.cpu().detach()
+# Plots result and target images
+def display_res(source, generated, target):
+    source = load_raster(source)
+    generated = load_raster(generated)
+    target = load_raster(target)
 
     plt.figure(figsize=(16, 6))
     ax1 = plt.subplot(1, 3, 1)
-    result = source_cpu[0][0]
-    plt.imshow(result.numpy())
+    plt.imshow(source)
     ax1.title.set_text("Source")
     plt.axis('off')
 
     ax2 = plt.subplot(1, 3, 2)
-    result = result_cpu[0]
-    plt.imshow(result.squeeze().numpy())
+    plt.imshow(generated)
     ax2.title.set_text("Model Result")
     plt.axis('off')
 
     ax3 = plt.subplot(1, 3, 3)
-    result = target_cpu[0]
-    plt.imshow(result.squeeze().numpy())
+    plt.imshow(target)
     ax3.title.set_text("Target Result")
     plt.axis('off')
 
     plt.set_cmap("viridis")
     plt.show()
+
+
+def load_raster(source):
+    with rasterio.open(source) as src:
+        arr = src.read(1)  # (H, W)
+    source = arr.astype(np.float32)
+    return source
